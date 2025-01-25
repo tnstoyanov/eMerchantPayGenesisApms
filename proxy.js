4c43921f5426b9cb1362e1964096daee43d5a014
tnstoyanov <tnstoyanov@gmail.com>
2025-01-25 23:27:35 +0200

    Update proxy.js
    const PORT = process.env.PORT || 3000;

--- proxy.js
+++ proxy.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse XML body
 app.use(bodyParser.text({ type: 'application/xml' }));
